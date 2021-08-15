35455ed66499f6255cf3e728cbcda6d4a564c36e
Mustafa Kamal <gistcodee@gmail.com>
2021-08-15 10:19:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// Example of how to fetch one card
+	useEffect(() => {
+		fetch('/card', { method: 'GET' })
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				// handle payload
+				console.log(data);
+			})
+			.catch((err) => {
+				// handle errors
+				console.log(err);
+			});
+	});
+	// Example of how to fetch many cards
+	useEffect(() => {
+		fetch('/cards', { method: 'GET' })
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				// handle payload
+				console.log(data);
+			})
+			.catch((err) => {
+				// handle errors
+				console.log(err);
+			});
+	});
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === 'development') {
+	const { worker } = require('./mocks/browser');
+	worker.start();
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
